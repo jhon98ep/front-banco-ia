187ce2f90447fd978f30eb96b0ac8af79a27b51b
jhon98ep <estradajhon07@gmail.com>
2024-01-24 16:10:52 -0500

    actualizar env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     url: 'http://localhost:8000/api/',
-    clave_openIA: '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',
+    clave_openIA: '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',
 };
